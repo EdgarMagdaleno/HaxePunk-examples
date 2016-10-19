323af2771383f2c06f1e80c819c333dc07fdd873
Edgar Magdaleno <edgarmv97@gmail.com>
2016-10-18 17:01:31 -0700

    Added screen scalation

--- cursors/src/MainScene.hx
+++ cursors/src/MainScene.hx
@@ -5,11 +5,14 @@ import com.haxepunk.HXP;
 import com.haxepunk.utils.Cursor;
 import com.haxepunk.utils.Input;
 import com.haxepunk.utils.Key;
+import com.haxepunk.screen.ScaleMode;
 
 class MainScene extends Scene
 {
 	public override function begin()
 	{
+		enableScale();
+
 		var blueBox = new Entity(114, 194, new Image("graphics/blueBox.png"));
 		blueBox.setHitbox(92, 92);
 		blueBox.type = "blueBox";
@@ -27,5 +30,11 @@ class MainScene extends Scene
 
 		HXP.cursor = new MyCursor();
 		Input.showCursor();
+	}
+
+	function enableScale() {
+		HXP.screen.scaleMode = new ScaleMode();
+		HXP.screen.scaleMode.setBaseSize(640, 480);
+		HXP.resize(HXP.windowWidth, HXP.windowHeight);
 	}
 }
